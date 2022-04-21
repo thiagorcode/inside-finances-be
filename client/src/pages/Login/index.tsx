81149aa5dcde6a11fa08cf08436eb635f9129369
Thiago Rodrigues de Oliveira <thiago.oliveira@bitgrow.com.br>
2022-04-21 17:42:46 -0300

    Small Adjust

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,42 +1,42 @@
 import React from "react";
 import './Style.css'
-import Moeda from './img/dollar.png'
-import vec1 from './img/Vector1.png'
-import vec2 from './img/Vector2.png'
-import baixo1 from './img/baixo1.png'
-import baixo2  from './img/baixo2.png'
-
-export default function login() {
-
-    return (
-        <body>
-            <div className="Arealogin">    
-            <img className="triangulo1" src={vec1} alt="triangulo" width="350px" />  
-            <img className="money" src={Moeda} alt="moeda" width="150px" />
-            <img className="tringbaixo1" src={baixo1} alt="triangulodebaixo" width="150px" />
-            </div>
-             <div className="Logoone" >
-             <img className="triangulo2" src={vec2} alt="triangulo2" width="355px" /> 
-             <img className="tringbaixo2" src={baixo2} alt="triangulodebaixo2" width="80px" />
-                 <h1>LOGIN</h1>
-             </div>
-             <div className="registerone">
-                 <form>
-                 <input type="text" placeholder="Email" autoFocus></input>
-                 </form>
-                 </div>
-             <div className="passowone">
-             <form>
-             <input type="password" placeholder="Senha" ></input>
-             <a href="http://localhost:3000/register"><p>cadastrar agora</p></a>
-                 </form>
-                  </div>
-             <div className="Login" >
-                 <form>
-                     <button>LOGIN</button>
-                     </form>
-             </div>
- </body>
-
-    )
+// Geralmente quando for importar usa letra maiúscula nas variáveis
+// TODO: Deixa assim depois te mostro outra maneira.
+import Coin from './img/dollar.png'
+import Vec1 from './img/Vector1.png'
+import Vec2 from './img/Vector2.png'
+import Down1 from './img/baixo1.png'
+import Down2 from './img/baixo2.png'
+/// Lembra de usar os nomes das functions com Letra maiúscula
+export default function Login() {
+  return (
+    <>
+      <div className="header-left">
+        <img className="triangle1" src={Vec1} alt="triangle" width="350px" />
+        <img className="money" src={Coin} alt="moeda" width="150px" />
+        <img className="tringdown1" src={Down1} alt="triangledown" width="150px" />
+      </div>
+      <div className="header-login" >
+        <img className="triangle" src={Vec2} alt="triangle2" width="355px" />
+        <img className="tringdow2" src={Down2} alt="triangledown2" width="80px" />
+        <h1>LOGIN</h1>
+      </div>
+      <form>
+        <div className="form-email">
+          <input type="text" placeholder="Email" autoFocus />
+        </div>
+        <div className="form-password">
+          <input type="password" placeholder="Senha" />
+          {/*
+          Depois te ensino a fazer direcionamento no React
+            Se quiser pesquisar vai no google e pesquisa "Link react router dom"
+          */}
+          <a href="http://localhost:3000/register"><p>cadastrar agora</p></a>
+        </div>
+        <div className="form-buttonsubmit" >
+          <button type="submit">LOGIN</button>
+        </div>
+      </form>
+    </>
+  )
 }
--- client/src/pages/Register/index.tsx
+++ client/src/pages/Register/index.tsx
@@ -3,52 +3,72 @@ import './style.css'
 import vec1 from '../Login/img/Vector1.png'
 import vec2 from '../Login/img/Vector2.png'
 import baixo1 from '../Login/img/baixo1.png'
-import baixo2  from '../Login/img/baixo2.png'
+import baixo2 from '../Login/img/baixo2.png'
 
-export default function login() {
+export default function Register() {
 
-    return (
-        <body>
-            <div className="Arearegister">    
-                   <img className="triangulo1" src={vec1} alt="triangulo" width="350px" />  
-                   <img className="tringbaixo1" src={baixo1} alt="triangulodebaixo" width="150px" />
-            </div>
-             <div className="Logo" >
-                     <img className="triangulo2" src={vec2} alt="triangulo2" width="355px" /> 
-                  <img className="tringbaixo2" src={baixo2} alt="triangulodebaixo2" width="80px" />
-                 <h1>Cadastrar</h1>
-             </div>
-             <div className="register">
-                     <form>
-                 <input type="text" placeholder="Email" autoFocus></input>
-                      </form>
-                 </div>
-             <div className="passow">
-                      <form>
-             <input type="password" placeholder="Senha" ></input>
-                        <a href="http://localhost:3000/login"><p>Voltar ao login</p></a> 
-                    </form>
-                  </div>
-             <div className="Login" >
-                    <form>
-                  <button>ENVIAR</button>
-                      </form>
-             </div>
-              <div className="Usuario">
-                     <form>
-                 <input type="text" placeholder="usuario" autoFocus></input>
-                     </form>
-                 </div>
-             <div className="passowcornfirm">
-                <form>
-             <input type="password" placeholder="Confirme sua senha" ></input>
-               </form>
-             </div>
+  return (
+    <>
+      <div className="Arearegister">
+        <img className="triangulo1" src={vec1} alt="triangulo" width="350px" />
+        <img className="tringbaixo1" src={baixo1} alt="triangulodebaixo" width="150px" />
+      </div>
+      <div className="Logo" >
+        <img className="triangulo2" src={vec2} alt="triangulo2" width="355px" />
+        <img className="tringbaixo2" src={baixo2} alt="triangulodebaixo2" width="80px" />
+        <h1>Cadastrar</h1>
+      </div>
+      {/* <div className="register">
+        <form>
+          <input type="text" placeholder="Email" autoFocus/>
+        </form>
+      </div>
+      <div className="passow">
+        <form>
+        <input type="password" placeholder="Senha" />
+        {/* Pode fazer igual a cima. */}
+      {/*Não precisa </input> <input type="password" placeholder="Senha" ></input>
+          <a href="http://localhost:3000/login"><p>Voltar ao login</p></a>
+        </form>
+      </div>
+      <div className="Login" >
+        <form>
+          <button type="button">ENVIAR</button>
+        </form>
+      </div>
+      <div className="Usuario">
+        <form>
+          <input type="text" placeholder="usuario" autoFocus/>
+        </form>
+      </div>
+      <div className="passowcornfirm">
+        <form>
+          <input type="password" placeholder="Confirme sua senha" />
+        </form>
+      </div> */}
+      {/* ********** */}
+      {/*! Você pode usar um form para tudo */}
+      <form>
 
-
-
-                 
- </body>
-
-    )
+        <div className="register">
+          <input type="text" placeholder="Email" autoFocus />
+        </div>
+        <div className="passow">
+          <input type="password" placeholder="Senha" />
+          {/* Pode fazer igual a cima. */}
+          {/*Não precisa colocar </input> <input type="password" placeholder="Senha" ></input> */}
+          <a href="http://localhost:3000/login"><p>Voltar ao login</p></a>
+        </div>
+        <div className="Login" >
+          <button type="button">ENVIAR</button>
+        </div>
+        <div className="Usuario">
+          <input type="text" placeholder="usuario" autoFocus />
+        </div>
+        <div className="passowcornfirm">
+          <input type="password" placeholder="Confirme sua senha" />
+        </div>
+      </form>
+    </>
+  )
 }
