b911af4b3bc8d5b3022e381eb0fb73a7930784a6
Thiago Rodrigues de Oliveira <thiago.oliveira@bitgrow.com.br>
2022-04-26 08:26:39 -0300

    Create function readSheet

--- script/index.js
+++ script/index.js
@@ -1,8 +1,26 @@
 // script para ler planilha excel e registrar os dados no sitema de controle de gasto.
-
-const request = require('request');
 const dotenv = require('dotenv');
+const fetch = require('node-fetch')
+const readXlsxFile = require('read-excel-file/node')
+// console.log(`${hostApi}/categorys`)
 
-const hostApi = 'http://localhost:3000'
-console.log(fetch(`${hostApi}/categorys`))
-request(`${hostApi}/categorys`, (err, res, body) => console.log(body))
+async function loadCategorys() {
+  console.log('Carregando categorias...');
+  const res = await fetch('http://localhost:3333/categorys');
+  const resJSON = await res.json()
+  return resJSON.categorys;
+}
+
+function readSheet() {
+  readXlsxFile('./testeScriptControle.xlsm', { sheet: 'Receita' }).then((row) => {
+    console.log(row)
+  })
+}
+
+async function start() {
+  const categorys = await loadCategorys()
+
+  readSheet()
+}
+
+start()
