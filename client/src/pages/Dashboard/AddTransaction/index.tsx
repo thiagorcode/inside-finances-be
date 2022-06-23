fb20af7459cb10242a768fe003148c76343c092e
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-06-22 22:24:59 -0300

    Add styyles

--- client/src/pages/Dashboard/AddTransaction/index.tsx
+++ client/src/pages/Dashboard/AddTransaction/index.tsx
@@ -1,4 +1,5 @@
 import { Close } from '@mui/icons-material';
+import { Box } from '@mui/system';
 import { useState } from 'react';
 import { Form } from './form';
 import { Container, Header } from './styles';
@@ -17,14 +18,42 @@ export const AddTransaction = ({ setIsVisibleModal }: AddTransactionProps) => {
   };
 
   return (
-    <Container>
-      <Header>
-        <button type="button" onClick={() => setIsVisibleModal(false)}>
-          <Close />
-        </button>
-        <h1>Adicionar Transação</h1>
-      </Header>
-      {Components[step]}
-    </Container>
+    <Box
+      component="div"
+      sx={{
+        maxWidth: '478px',
+        ml: 'auto',
+        mr: 'auto',
+        backgroundColor: 'transparent',
+        position: 'absolute',
+        bottom: 0,
+        left: '50%',
+        transform: 'translate(-50%, 0)',
+        height: '87vh',
+        width: '100%',
+      }}
+    >
+      <Box
+        component="div"
+        bgcolor="#2B2E35"
+        sx={{
+          display: 'block',
+          px: 2.3,
+          py: 2,
+          mb: 2,
+          borderRadius: 6,
+        }}
+      >
+        <Container>
+          <Header>
+            <button type="button" onClick={() => setIsVisibleModal(false)}>
+              <Close />
+            </button>
+            <h1>Adicionar Transação</h1>
+          </Header>
+          {Components[step]}
+        </Container>
+      </Box>
+    </Box>
   );
 };
