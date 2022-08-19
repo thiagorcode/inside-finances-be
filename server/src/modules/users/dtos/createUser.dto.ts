a192d70c5bcd8238d8195bbe48b705ff3cfc967e
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-08-19 19:29:18 -0300

    refactor: add new dto

---
+++ server/src/modules/users/dtos/createUser.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDTO {
+  @ApiProperty()
+  username: string;
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  password: string;
+  @ApiProperty()
+  isActive: boolean;
+}
