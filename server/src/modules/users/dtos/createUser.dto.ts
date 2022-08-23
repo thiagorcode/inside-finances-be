3c8fd842f877fde8dcfacf7ba13ca0e75e9404d0
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-08-23 17:40:48 -0300

    Add space

--- server/src/modules/users/dtos/createUser.dto.ts
+++ server/src/modules/users/dtos/createUser.dto.ts
@@ -3,10 +3,13 @@ import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDTO {
   @ApiProperty()
   username: string;
+
   @ApiProperty()
   email: string;
+
   @ApiProperty()
   password: string;
+
   @ApiProperty()
   isActive: boolean;
 }
