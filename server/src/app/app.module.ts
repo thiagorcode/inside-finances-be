3f59eb8e86ce05c1ce10d476345247b1ff41805e
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-10-07 23:38:47 -0300

    feat: add config service api

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ApiConfigService } from './../shared/services/api-config.service';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,15 +7,18 @@ import { AuthModule } from '../modules/auth/auth.module';
 import { UsersModule } from '../modules/users/users.module';
 import { TransactionsModule } from '../modules/transactions/transactions.module';
 import { TransactionsCategoryModule } from '../modules/transactionsCategory/transactionsCategory.module';
-import { TypeOrmConfigService } from '../infra/database/typeorm-config.service';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useClass: TypeOrmConfigService,
+      imports: [SharedModule],
+      useFactory: (configService: ApiConfigService) => configService.ormConfig,
+      inject: [ApiConfigService],
     }),
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }),
     AuthModule,
     UsersModule,
---
+++ server/src/shared/services/api-config.service.ts
@@ -0,0 +1,97 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { isNil } from 'lodash';
+
+@Injectable()
+export class ApiConfigService {
+  constructor(private configService: ConfigService) {}
+
+  get isDevelopment(): boolean {
+    return this.nodeEnv === 'development';
+  }
+
+  get isProduction(): boolean {
+    return this.nodeEnv === 'production';
+  }
+
+  get isTest(): boolean {
+    return this.nodeEnv === 'test';
+  }
+
+  private getNumber(key: string): number {
+    const value = this.get(key);
+
+    try {
+      return Number(value);
+    } catch {
+      throw new Error(key + ' environment variable is not a number');
+    }
+  }
+
+  private getBoolean(key: string): boolean {
+    const value = this.get(key);
+
+    try {
+      return Boolean(JSON.parse(value));
+    } catch {
+      throw new Error(key + ' env var is not a boolean');
+    }
+  }
+
+  private getString(key: string): string {
+    const value = this.get(key);
+
+    return value.replace(/\\n/g, '\n');
+  }
+
+  get nodeEnv(): string {
+    return this.getString('NODE_ENV');
+  }
+
+  get ormConfig(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.getString('MYSQL_HOST'),
+      port: this.getNumber('MYSQL_PORT'),
+      username: this.getString('MYSQL_USERNAME'),
+      password: this.getString('MYSQL_PASSWORD'),
+      database: this.getString('MYSQL_DATABASE'),
+      synchronize: this.isTest,
+      entities: ['dist/modules/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/**/*{.ts,.js}'],
+      autoLoadEntities: true,
+      keepConnectionAlive: !this.isTest,
+      dropSchema: this.isTest,
+      name: 'default',
+      // subscribers: [UserSubscriber],
+      migrationsRun: true,
+      logging: this.getBoolean('ENABLE_ORM_LOGS'),
+      // namingStrategy: new SnakeNamingStrategy(),
+    };
+  }
+
+  get authConfig() {
+    return {
+      privateKey: this.getString('JWT_PRIVATE_KEY'),
+      publicKey: this.getString('JWT_PUBLIC_KEY'),
+      jwtExpirationTime: this.getNumber('JWT_EXPIRATION_TIME'),
+    };
+  }
+
+  get appConfig() {
+    return {
+      port: this.getString('PORT'),
+    };
+  }
+
+  private get(key: string): string {
+    const value = this.configService.get<string>(key);
+
+    if (isNil(value)) {
+      throw new Error(key + ' environment variable does not set'); // probably we should call process.exit() too to avoid locking the service
+    }
+
+    return value;
+  }
+}
