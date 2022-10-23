d945d23e95d087e9f96019dac55a9bb20af54051
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-10-23 14:17:34 -0300

    feat: add rate limit

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+import { rateLimit } from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,10 +11,18 @@ async function bootstrap() {
     .setDescription('The app API description')
     .setVersion('0.2.0')
     .addTag('app')
+    .setContact('Thiago Rodrigues', '', 'ti.thiago.rodrigues@gmail.com')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 1000, // limit each IP to 10000 requests per windowMs
+    }),
+  );
 
   await app.listen(process.env.PORT);
 
