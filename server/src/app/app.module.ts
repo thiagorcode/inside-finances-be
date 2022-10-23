a23140fed5d7be28af1e5a7e17fdbe4e7109e158
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-10-23 14:18:03 -0300

    feat: add dataSource in factory typeorm

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { ApiConfigService } from './../shared/services/api-config.service';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { AuthModule } from '../modules/auth/auth.module';
+import { ApiConfigService } from './../shared/services/api-config.service';
 
 import { UsersModule } from '../modules/users/users.module';
 import { TransactionsModule } from '../modules/transactions/transactions.module';
@@ -11,14 +12,18 @@ import { SharedModule } from '../shared/shared.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRootAsync({
       imports: [SharedModule],
       useFactory: (configService: ApiConfigService) => configService.ormConfig,
       inject: [ApiConfigService],
-    }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
     }),
     AuthModule,
     UsersModule,
