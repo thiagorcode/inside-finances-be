2ecffdf593ab61d45c58d62a962e0c8acf9fe04c
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-10-26 21:49:37 -0300

    remove client

--- client/README.md
+++
@@ -1,2 +0,0 @@
-# template_react
-Template React with Vite, ESLINT, Prettier and Typescript
--- client/src/helpers/formatHelpers.ts
+++
@@ -1,7 +0,0 @@
-const formatter = Intl.NumberFormat('pt-BR');
-
-function formatNumber(value: number) {
-  return formatter.format(value);
-}
-
-export default { formatNumber };
