1c224eacbb2344463ffdfe389513fa6ab75383a8
Thiago Rodrigues <ti.thiago.rodrigues@gmail.com>
2022-11-04 07:59:20 -0300

    feat: active cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { rateLimit } from 'express-rate-limit';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
